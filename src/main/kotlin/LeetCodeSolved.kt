dcf14addbe85d6aac01223e942460a59450730b1
doubleo <greg_osagie@yahoo.com>
2025-03-18 11:58:22 +0000

    combinationSum3

--- src/main/kotlin/LeetCodeSolved.kt
+++ src/main/kotlin/LeetCodeSolved.kt
@@ -2,24 +2,28 @@ import java.util.*
 import kotlin.math.max
 
 class LeetCodeSolved {
+    fun combinationSum3(k: Int, n: Int): List<List<Int>> {
 
+        val candidates= (1..9).toList()
 
+        val ans= mutableListOf<List<Int>>()
 
-        fun combinationSum2(candidates: IntArray, target: Int): List<List<Int>> {
-            candidates.sort()  // Sorting helps in skipping duplicates
-            val ans = mutableListOf<List<Int>>()
-            backtrack(mutableListOf(), 0, ans, candidates, target)
-            return ans
-        }
+        backtrack(mutableListOf(),0,ans,candidates,n,k)
+        println(ans)
+
+        return ans
+
+    }
 
         private fun backtrack(
             curr: MutableList<Int>,
             index: Int,
             ans: MutableList<List<Int>>,
-            candidates: IntArray,
-            target: Int
+            candidates: List<Int>,
+            target: Int,
+            k:Int
         ) {
-            if (target == 0) {  // Base case: when remaining sum is zero
+            if (target == 0 && curr.size==k) {  // Base case: when remaining sum is zero
                 ans.add(ArrayList(curr))
                 return
             }
@@ -31,11 +35,10 @@ class LeetCodeSolved {
                 if (candidates[i] > target) break // Stop early (pruning)
 
                 curr.add(candidates[i])
-                backtrack(curr, i + 1, ans, candidates, target - candidates[i]) // Move to next index
+                backtrack(curr, i + 1, ans, candidates, target - candidates[i],k) // Move to next index
                 curr.removeAt(curr.size - 1)  // Backtrack step
             }
         }
-
 
 
     data class TreeNode(
--- src/test/kotlin/LeetCodeSolvedTest.kt
+++ src/test/kotlin/LeetCodeSolvedTest.kt
@@ -4,18 +4,19 @@ import kotlin.test.assertEquals
 class LeetCodeSolvedTest {
 
   @Test
-  fun combinationSum2(){
+  fun combinationSum3(){
 //      given
-      val candidates = intArrayOf( 10,1,2,7,6,1,5)
-      val target = 8
+
+      val k= 3
+      val n=9
 
 //      when
 
-      val result:List<List<Int>> = LeetCodeSolved().combinationSum2(candidates,target)
+      val result:List<List<Int>> = LeetCodeSolved().combinationSum3(k,n)
 
 
 //      then
-      val expectation = listOf(listOf(1,1,6), listOf(1,2,5), listOf(1,7), listOf(2,6))
+      val expectation = listOf(listOf(1,2,6), listOf(1,3,5), listOf(2,3,4))
 
       assertEquals(expectation,result)
 
