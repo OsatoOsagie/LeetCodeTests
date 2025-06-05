c1d974c892db54357c899d12c43489d9881cdd3f
Your Name <your.email@example.com>
2025-06-05 12:26:56 +0100

    test

--- src/main/kotlin/LeetCodeSolved.kt
+++ src/main/kotlin/LeetCodeSolved.kt
@@ -5,26 +5,7 @@ import kotlin.math.absoluteValue
 import kotlin.math.sign
 
 class LeetCodeSolved {
-    fun merge(intervals: Array<IntArray>): Array<IntArray> {
 
-        val sorted= intervals.sortedBy{it[0]}
-
-        val ans= mutableListOf<IntArray>()
-
-        for (s in sorted){
-            val startTime = s[0]
-            val endTime= s[1]
-            if (ans.isNotEmpty() && startTime <= ans.last()[1]){
-
-                ans.last()[1]= maxOf(endTime, ans.last()[1])
-
-            }else{
-                ans.add(s)
-            }
-        }
-
-        return ans.toTypedArray()
-    }
 
 
 }
--- src/test/kotlin/LeetCodeSolvedTest.kt
+++ src/test/kotlin/LeetCodeSolvedTest.kt
@@ -5,20 +5,7 @@ import kotlin.test.assertEquals
 
 class LeetCodeSolvedTest {
 
-    @Test
-    fun merge(){
-//        given
-        val intervals = arrayOf(intArrayOf(1,3), intArrayOf(2,6), intArrayOf(8,10), intArrayOf(15,18))
 
-//        when
-        val result:Array<IntArray> = LeetCodeSolved().merge(intervals)
-
-//        then
-        val expectation = arrayOf(intArrayOf(1,6), intArrayOf(8,10), intArrayOf(15,18))
-
-        assertTrue(expectation.contentDeepEquals(result), "The nested arrays are not equal.")
-
-    }
 
 
 
